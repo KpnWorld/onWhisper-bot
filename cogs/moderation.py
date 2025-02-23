dfae7d82d5f9b2f891c4efccb6a5f7731cfd9965
KpnWorld <og.devworld@gmail.com>
2025-02-23 18:43:22 -0500

    Test

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -110,13 +110,9 @@ class Moderation(commands.Cog):
             embed.add_field(name=formatted_time, value=reason, inline=False)
         await interaction.response.send_message(embed=embed)
 
-    class Whisper(commands.Cog):
-        def __init__(self, bot):
-            self.bot = bot
-        
-        @app_commands.command(name="test", description="test if whisper cog is working")
-        async def test(self, interaction: discord.Interaction):
-            await interaction.response.send_message("Whisper cog is working")
+    @app_commands.command(name="test", description="test if whisper cog is working")
+    async def test(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Whisper cog is working")
 
 
 async def setup(bot):
